0e6e67c3bcdb1027431374ca5f548520ab65e7ec
lulumbp15 <luoweizhou23@163.com>
2018-09-03 15:31:14 +0800

    RGB

---
+++ DynamicSelection/Assets/ColorManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour {
+
+    public enum ManipulatedObjectColor
+    {
+        Red,
+        Blue,
+        Green
+    }
+
+    GameObject m_object;
+    Renderer rend;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        m_object = DetectionManager.Get().GetCurrentGameObject();
+        rend = m_object.GetComponent<Renderer>();
+    }
+
+    public void ChangetoRedColor()
+    {
+        rend.material.color = Color.red;
+    }
+
+    public void ChangetoGreenColor()
+    {
+        rend.material.color = Color.green;
+    }
+
+    public void ChangetoBlueColor()
+    {
+        rend.material.color = Color.blue;
+    }
+
+
+}
