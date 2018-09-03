3808c8e9c4e2d224f09c69b9626c1fd78931fb06
lulumbp15 <luoweizhou23@163.com>
2018-09-03 12:13:00 +0800

    ChangetoHalfSize

---
+++ DynamicSelection/Assets/ChangeSizetoS.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSizetoS : MonoBehaviour {
+   
+    GameObject m_object = DetectionManager.Get().GetCurrentGameObject();
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //ChangetoHalfSizem();
+	}
+
+    public void ChangetoHalfSize()
+    {
+        m_object.transform.localScale = new Vector3(m_object.transform.localScale.x * 0.5f, m_object.transform.localScale.x * 0.5f, m_object.transform.localScale.x * 0.5f);
+    }
+}
